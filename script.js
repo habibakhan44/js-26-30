c55dc41306ad19699bfbf8ba3ad4f3c4b8ad794d
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2024-11-16 21:56:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// QUESTION 1
+// let positivenum=prompt("Enter a postive Number:");
+// document.write("Number is:" + positivenum + "<br>");
+// document.write("Round off a number:" + Math.round(positivenum) + "<br>");
+// document.write("Floor value:" + Math.floor(positivenum) + "<br>");
+// document.write("Ceil Value:" + Math.ceil(positivenum));
+
+// QUESTION 2
+// let positivenum=prompt("Enter a negative Number:");
+// document.write("Number is:" + positivenum + "<br>");
+// document.write("Round off a number:" + Math.round(positivenum) + "<br>");
+// document.write("Floor value:" + Math.floor(positivenum) + "<br>");
+// document.write("Ceil Value:" + Math.ceil(positivenum));
+
+// QUESTION 3
+// let number = -4;
+// document.write("The absolute of " + number + " is " + Math.abs(number));
+
+// QUESTION 4
+// let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// document.write("random dice value: "  +  randomNumber1  +  "<br>");
+// document.write("random dice value: "  +  randomNumber2);
+
+// QUESTION 5
+// let coin = Math.floor(Math.random() * 2) + 1;
+// if (coin === 1) {
+//     document.write(coin + "<br>random coin value: Tails");
+// } else if(coin===2) {
+//             document.write(coin + "<br>random coin value: Heads");
+//     }
+
+// QUESTION 6
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + randomNumber);
+
+// QUESTION 7
+// let weight = prompt("Enter your weight in kilograms:");
+// let userweight = parseFloat(weight); 
+// document.write("The weight of the user is " + userweight + " kilograms.");
+
+// QUESTION 8
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = prompt("Guess the secret number between 1 to 10:");
+// if (userGuess === secretNumber) {
+//      alert("Congratulations! You guessed the secret number: " + secretNumber);
+//     } else {
+//         alert("Try again ");
+//         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
